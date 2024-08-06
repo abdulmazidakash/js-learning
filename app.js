c35e652563023829fcb47a3192e380dbab90f368
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-07 00:12:50 +0600

    learning js statements

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert('hi javascript')
+// alert('hi javascript');
+
+// console.log('hello world');
+
+// document.getElementById('hello').innerHTML = 'hello world'; 
+
+let sum = 150 + 40;
+console.log(sum);
+
+if (sum > 100){
+	console.log('expensive');
+}
+else{
+	console.log('cheep');
+}
+
+function sayHello(name){
+	console.log('Hello &{name}');
+}
+sayHello('Akash');
