fdc74c15c0f0c592659d2aa1584aac657c63a85f
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-09 09:31:03 +0600

    learning math js

---
+++ math.js
@@ -0,0 +1,29 @@
+let num = 30.345345;
+
+
+console.log(num);
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+
+
+// square value 
+console.log(Math.pow(4, 2));
+
+// root value 
+console.log(Math.sqrt(64));
+
+// positive value output
+console.log(Math.abs(-5));
+
+// find max and min 
+console.log(Math.max(3,2,4,6,7,9,1));
+console.log(Math.min(3,2,4,6,7,9,1));
+
+// sin cos value 
+console.log(Math.sin((0 * Math.PI) / 180));
+console.log(Math.cos((0 * Math.PI) / 180));
+
+// random number result 
+console.log(Math.random().toFixed(2) * 10 + 2);
+console.log(Math.floor (Math.random().toFixed(2) * 10 + 2));
