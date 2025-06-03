6182889330a71ae66e7b55061b8b07a6d2003666
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-06-03 10:07:07 +0600

    cash out successful

--- payoo/payoo-home.js
+++ payoo/payoo-home.js
@@ -1,6 +1,43 @@
 // console.log('payoo home page loaded');
+// add money section 
 document.getElementById('btn-add-money').addEventListener('click', function(e){
 	const addMoney = document.getElementById('input-add-money').value ;
 	const pinNumber = document.getElementById('input-pin-number').value ;
-	console.log(addMoney, pinNumber);
+
+	if(pinNumber === '1234'){
+		// console.log('pin successful');
+		const accountBalance = document.getElementById('account-balance').innerText;
+		const numberAccountBalance = parseFloat(accountBalance);
+		const numberAddMoney = parseFloat(addMoney);
+		const newBalance = numberAccountBalance + numberAddMoney;
+		// console.log(newBalance);
+		document.getElementById('account-balance').innerText = newBalance;
+
+	}else{
+		alert('wrong pin number or invalid amount');
+		return;
+	}
+
+});
+
+// cash out section 
+document.getElementById('btn-cash-out').addEventListener('click', function(e){
+	const cashOutAmount = document.getElementById('input-cash-out').value ;
+	const pinNumber = document.getElementById('input-cash-out-pin-number').value ;
+	// console.log(cashOutAmount, pinNumber);
+
+	if(pinNumber === '1234'){
+		// console.log('pin successful');
+		const accountBalance = document.getElementById('account-balance').innerText;
+		const numberAccountBalance = parseFloat(accountBalance);
+		const numberCashOutBalance = parseFloat(cashOutAmount);
+		const newBalance = numberAccountBalance - numberCashOutBalance;
+
+		document.getElementById('account-balance').innerText = newBalance;
+	}else{
+		console.log('wrong pin number');
+
+	}
+
 })
+
