66d922ed4381554d8f3cc3c84a8a287c59e88635
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-06-06 11:15:09 +0600

    module 31 compelte

--- es6.js
+++
@@ -1,3 +0,0 @@
-let money = 25;
-money = 50;
-console.log(money);
--- es6/es6.js
+++ es6/es6.js
@@ -1,11 +1,86 @@
 // let money = 25;
 // money = 50;
 // console.log(money);
-function add(x, y){
-	return x + y;
-}
-const sum = add(5, 10);
+// function add(x, y){
+// 	return x + y;
+// }
+// const sum = add(5, 10);
+// console.log(sum);
+// template literals ---------
+// const sum1 = `total sum ${sum}`;
+// console.log(sum1);
+// arrow functions -----------
+// const getAge = (person) => person.age;
+// const student = { name: 'akash', age: 45};
+// const age = getAge(student);
+// console.log(age);
+// spread operator -------------
+// const one = [3, 5,];
+// const two = [4, 6];
+// const combined = [...one, ...two];
+// console.log(combined);
+// const person = {
+// 	name: 'akash',
+// 	age: 45,
+// }
+
+// const person1 = {
+// 	brother: 'shakib',
+// 	age1: 40,
+// }
+
+// const people = {...person, ...person1};
+// console.log(people);
+// const numbers = [1,45, 3,4 ,5, 5, 40,];
+// const map = numbers.find((num)=>num >4 );
+// console.log(map);
+
+// class Techer{
+// 	constructor(name, subject){
+// 		this.name = name;
+// 		this.subject = subject;
+// 	}
+// 	lecture(){
+// 		console.log('sir is teaching math');
+// 	}
+// }
+// const teacher = new Techer('akash', 'math');
+// console.log(teacher);
+
+// class vehicle{
+// 	constructor(name, price){
+// 		this.name = name;
+// 		this.price = price;
+// 	}
+// 	move(){
+// 		console.log('bus is moving');
+// 	}
+// }
+
+// class Bus extends vehicle{
+// 	constructor(name, price, seats){
+// 		super(name, price);
+// 		this.seats = seats;
+// 	}
+// 	move(){
+// 		console.log('bus is moving fast');
+// 	}
+// }
+
+// const bus = new Bus('volvo', 5000, 40);
+// console.log(bus);
+
+const peoples = [
+	{ name: 'meena',  age: 20},
+	{ name: 'rina',  age: 15},
+	{ name: 'suchorita',  age: 22}
+];
+// let sum = 0;
+// for(let people of peoples){
+// 	sum += people.age;
+// 	// console.log(sum);
+// }
 // console.log(sum);
 
-const sum1 = `total sum ${sum}`;
-console.log(sum1);
+const totalAge = peoples.reduce((p, c)=> p + c.age, 0);
+console.log(totalAge);
