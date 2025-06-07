82a9f3e6cd15fe39de3fe05a3e782c17a3998e78
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-06-07 23:01:41 +0600

    callback funcion

--- es6/es6.js
+++ es6/es6.js
@@ -70,17 +70,110 @@
 // const bus = new Bus('volvo', 5000, 40);
 // console.log(bus);
 
-const peoples = [
-	{ name: 'meena',  age: 20},
-	{ name: 'rina',  age: 15},
-	{ name: 'suchorita',  age: 22}
-];
-// let sum = 0;
-// for(let people of peoples){
-// 	sum += people.age;
-// 	// console.log(sum);
-// }
-// console.log(sum);
+// const peoples = [
+// 	{ name: 'meena',  age: 20},
+// 	{ name: 'rina',  age: 15},
+// 	{ name: 'suchorita',  age: 22}
+// ];
+// // let sum = 0;
+// // for(let people of peoples){
+// // 	sum += people.age;
+// // 	// console.log(sum);
+// // }
+// // console.log(sum);
 
-const totalAge = peoples.reduce((p, c)=> p + c.age, 0);
-console.log(totalAge);
+// const totalAge = peoples.reduce((p, c)=> p + c.age, 0);
+// console.log(totalAge);
+
+// const x = 0;
+// if(x){
+// 	console.log('this is true');
+// }else{
+// 	console.log('this is false');
+// }
+// const x = 0;
+// const y = false;
+// if(x == y){
+// 	console.log('values are equal');
+// }else{
+// 	console.log('values are not equal');
+// }
+
+// function add(a, b){
+// 	const total = a + b;
+// 	console.log(a + b);;
+// 	if(b > 5){
+// 		const sum = 25 + a + b;
+// 	}else{
+// 		var sum = 10 + a + b;
+// 	}
+// 	console.log(sum);
+// 	return total;
+// }
+// hoisting 
+// add(6, 20);
+// print5();
+// function print5(){
+// 	console.log('print 5');
+// }
+
+// for(let i = 0; i< 10; i++){
+// 	console.log(i);
+// }
+// console.log(i);
+// function kitchen(){
+// 	let roast = 0;
+// 	return function(){
+// 		roast++;
+// 		return roast;
+// 	}
+// };
+// const firstServer = kitchen();
+// console.log(firstServer());
+// console.log(firstServer());
+// console.log(firstServer());
+// console.log(firstServer());
+// console.log(firstServer());
+// console.log(firstServer());
+
+// closure 
+// function stopWatch(){
+// 	let counter = 0;
+// 	return function(){
+// 		counter++;
+// 		return counter;
+// 	}
+// }
+
+// const watch1 = stopWatch();
+// const watch2 = stopWatch();
+// console.log(watch1());
+// console.log(watch1());
+// console.log(watch1());
+// console.log(watch1());
+// console.log(watch1());
+// console.log('watch 2--->', watch2());
+// console.log('watch 2--->', watch2());
+// console.log('watch 2--->', watch2());
+// console.log('watch 2--->', watch2());
+
+//callback function
+function greeting(greetingHandler, name){
+	greetingHandler(name);
+}
+
+function greetingHandler(name){
+	console.log('call greeting handler for', name);
+}
+
+function goodNight(name){
+	console.log('Good night', name);
+}
+function goodMorning(name){
+	console.log('Good night', name);
+}
+
+greeting(greetingHandler, 'akash');
+greeting(goodNight, 'akash');
+greeting(goodMorning, 'akash');
+
